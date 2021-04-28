00fd4ff62d25490961236f1f6d9e97b9815a81ef
esnonso <devnellynonso@gmail.com>
2021-04-28 20:12:54 +0100

    Make + button work to add new ing. Edit styles to reciprForm

--- src/recipeForm.js
+++ src/recipeForm.js
@@ -11,85 +11,96 @@ class Form extends Component {
             {
                 title:"",
                 instructions:"",
-                ingridients:[],
+                ingridients:[''],
                 img:""
             }
         ]}
         this.handleChange = this.handleChange.bind(this);
-        this.handleClick = this.handleChange.bind(this);
+        this.handleNewIngridient = this.handleNewIngridient.bind(this);
+        this.handleChangeIng = this.handleChangeIng.bind(this)
     }
 
     handleChange(e) {
         this.setState({[e.target.name]:e.targe.value})
     }
 
-    handleClick(e){
-        const ingridients = [...this.state.ingridients]
-        this.setState({ingridients:ingridients})
+    handleNewIngridient(e){
+        const {ingridients} = this.state
+        this.setState({ingridients:[...ingridients, ""]})
+    }
+
+    handleChangeIng(e){
+        const index = Number(e.target.name.split('-')[1]) // THIS GETS THE INDEX OF THE ARRAY WE ARE ON
+        /**Number()=> Tells React we want a number not string
+         * split('-') => spilt the string at the dash and getting the second element in the array which is 
+         * the index
+         */
+        const ingridients = this.state.ingridients.map((ing, i) => (
+            i===index ? e.target.value : ing //if i is equal to index return the value of that index else return the ing.
+            ))
+            this.setState({ingridients})
     }
    
     render(){
         const { title, instructions, ingridients, image} = this.state;
-        const onClose = this.props
-        let inputs = this.state.ingridients.map((ing, index)=> (
-            <div><input
+        const {onClose} = this.props
+        let inputs = ingridients.map((ing, index)=> (
+            <div className="field-wrap" key={`ingrideints-${index}`}>
+            <label>{index + 1}</label>
+            <input
              type="text" 
-              key={`ingridients ${index}`}
-              name={`ingridients ${index}`}
-              //value={ing}
-              size={45}
+              name={`ingridients-${index}`}
+              value={ing}
+              size={30}
               autoComplete ="off"
-              placeholder="ingridients"></input>
+              placeholder="ingridients"
+              onChange ={this.handleChangeIng}></input>
+              
             </div>
         ))
         return(
             <form>
-                <div class="field-wrap" style={{display:"flex" ,flexDirection:"row-reverse"}}>
+                <div className="field-wrap" style={{display:"flex" ,flexDirection:"row-reverse"}}>
             <button type="button"
                 className="close-btn"
                 onClick = {onClose}
                 style={{backgroundColor:"gray"}}>X
             </button>
             </div>
-            <div class="field-wrap">
-                <label for="title">Title: </label>
+            <div className="field-wrap">
+                <label >Title: </label>
                 <input type="text"
                 key= "title"
                 name="title"
                 value={title}
-                size={45}
+                size={30}
                 autoComplete ="off"
                 onChange={this.handleChange}
                  />
             </div>
 
-            <div class="field-wrap">
+            <div className="field-wrap">
                 <label><h5>Instructions</h5>
                     <textarea rows="7"
                     key= "instructions"
                     name="instructions"
                     value={instructions}
-                    size={45}
+                    size={30}
                     onChange={this.handleChange}
                     autoComplete ="off"
                     />
                 </label>
             </div>
-
-            <div class="field-wrap">
-                <ol>
                     {inputs} 
-                </ol>
-            </div>
 
-            <div className="btnDiv"><button className="add" onClick={this.handleClick}>+</button></div>
+            <div className="btnDiv"><button type="button" className="add" onClick={this.handleNewIngridient}>+</button></div>
 
-            <div class="field-wrap">
-                <label for="img">ImageUrl: </label>
+            <div className="field-wrap">
+                <label >ImageUrl: </label>
                 <input name="image" 
                 key= "image"
                 value={image}
-                size={45}
+                size={30}
                 autoComplete ="off"
                 onChange={this.handleChange}/>
             </div>
