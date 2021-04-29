812b744f0e3297d9903a07401224e5fbb7d650ba
esnonso <devnellynonso@gmail.com>
2021-04-29 13:34:11 +0100

    more change commited

--- src/recipe.js
+++ src/recipe.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import './recipe.css'
+import './recipe.css';
+import PropTypes from 'prop-types'
 
 class Recipe extends  Component  {
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        ingridients: PropTypes.arrayOf(PropTypes.string).isRequired,
+        instructions:PropTypes.string.isRequired,
+        image:PropTypes.string.isRequired
+       
+    }
     render() {
-        const { title, instructions } = this.props; //Destructuring
-        const ingridients = this.props.ingridients.map((ing, index) => (
+        const { title, instructions, image } = this.props; //Destructuring
+        const ingridient = this.props.ingridients.map((ing, index) => (
             <li key={index}>{ing}</li>
-        ))
+       ))
+        
         return (
             <div >
                 <div className ="recipeDiv">
-            <img src={process.env.PUBLIC_URL + '/rice.png'} alt="logo" />
+            <img src={image} alt="logo" />
             <h5>{title}</h5>
-            <ul><h5>Ingridients:</h5>{ingridients}</ul>
+            <ol>{ingridient}</ol>
             {instructions}
             </div>
             </div>
